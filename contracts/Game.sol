f51ee463e63e9ab1d19575ad7e565749cff2c803
fzovpec <fzovpec2@gmail.com>
2023-04-21 13:53:40 +0200

    Contract

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -7,9 +7,20 @@ contract Game {
         uint256 losses;
     }
 
+    address owner;
+    
+    constructor() {
+        owner = msg.sender; // setting the owner the contract deployer. Can be reset to another address
+    }
+
     mapping(bytes32 => mapping(uint256 => Scores)) private scores;
 
-    function updateScores(bytes32 username, uint256 gameId, uint256 wins, uint256 losses) public {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract creator can send requests");
+        _;
+    }
+
+    function updateScores(bytes32 username, uint256 gameId, uint256 wins, uint256 losses) public onlyOwner {
         scores[username][gameId].wins += wins;
         scores[username][gameId].losses += losses;
     }
